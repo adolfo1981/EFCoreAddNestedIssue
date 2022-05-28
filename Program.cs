ccbb1d6e9a4da4b6d569fa36f05233b652e4dea0
Adolfo Perez <aperez@fingercheck.com>
2022-05-28 10:34:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNestedIssue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddCompanyWithEmployeeTest();
+        }
+
+        private static void AddCompanyWithEmployeeTest()
+        {
+            using (var context = new EFCoreTestContext())
+            {
+                var company = new Company
+                {
+                    Id = -1, //Our presentation layer sets Id to -1 for new entities
+                    Name = "Tesla",
+                    Employees = new List<Employee> {
+                        new Employee {
+                            Id = -1, //Our presentation layer sets Id to -1 for new entities
+                            Name = "Bob Ross" }
+                    }
+                };
+
+                context.Company.Add(company);
+                context.SaveChanges();
+            }
+        }
+    }
+}
